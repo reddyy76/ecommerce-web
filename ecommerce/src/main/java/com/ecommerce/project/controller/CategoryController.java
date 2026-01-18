079f337ad39f99ec9cffec04eaab46fd0da6ad52
N. Rahul Reddy <rahulreddydec31@gmail.com>
2026-01-18 11:59:32 +0530

    Initial commit

---
+++ ecommerce/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,54 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/public/categories")
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @PostMapping("/public/categories")
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
+        categoryService.createCategory(category);
+        return new ResponseEntity<>("Category created successfully", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
+
+    @PutMapping("/public/categories/{categoryId}")
+    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+                                                 @PathVariable Long categoryId) {
+        try{
+            Category savedCategory = categoryService.updateCategory(category, categoryId);
+            return new ResponseEntity<>("Category updated successfully " + categoryId , HttpStatus.OK);
+        }catch (ResponseStatusException e){
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
+    }
+}
---
+++ ecommerce/src/main/java/com/ecommerce/project/service/CatergoryServiceImpl.java
@@ -0,0 +1,49 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CatergoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+        categoryRepository.delete(category);
+        return "Deleted Category with categoryId : " + categoryId + " deleted successfully";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+        category.setCategoryId(categoryId);
+        savedCategory =  categoryRepository.save(category);
+        return savedCategory;
+    }
+}
